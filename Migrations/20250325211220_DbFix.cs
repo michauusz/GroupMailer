d8b94f16c591ff93acd6528e9e48cbc0732c1ae6
michaues <michal.pawlowski1997@gmail.com>
2025-03-25 22:15:38 +0100

    migration add

---
+++ Migrations/20250325211220_DbFix.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GroupMailer.Migrations
+{
+    /// <inheritdoc />
+    public partial class DbFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailList",
+                table: "EmailCampaigns",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailList",
+                table: "EmailCampaigns");
+        }
+    }
+}
